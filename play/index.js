e5bcaa67d5dfc7d168bf087bbda86e33db701a56
Rohit Nandan <pulkitnandan@gmail.com>
2017-04-01 20:56:37 +0530

    Adding footer story at vueplay

--- play/index.js
+++ play/index.js
@@ -1,6 +1,10 @@
 import { play } from 'vue-play'
+import DBSFooter from '../components/DBSFooter'
 
 play('MyButton')
   .add('with text', {
     template: `<button @click="$log('123')">Hello</button>`
   })
+
+  play('Footer')
+    .add('Normal', h => h(DBSFooter))
